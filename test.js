ba9c0df71e01418e3a46989a11c649ebf59f0b7c
Geordan Rosario <geordan@gmail.com>
2018-09-08 16:11:36 -0700

    Add Republic/Separatist faction icons.
    Fix tests to use xwing-data2.
    
    fixes #57
    fixes #58
    fixes #59

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var fetch = require('node-fetch')
 var fontkit = require('fontkit');
 var colors = require('colors');
 
@@ -31,53 +31,62 @@ exports['test ship glyphs'] = function(assert) {
     assertGlyphs('xwing-miniatures-ships', 'ships', assert);
 };
 
-const shipAliases = [
-    'ig2000',
-    'tieadvancedprototype',
-    'yt2400freighter',
-];
+function canonicalize(s) {
+    return s.toLowerCase().replace(/[^a-z0-9]/g, '')
+}
 
 exports['test ship names'] = function(assert, done) {
+    // Epic ships to ignore (for now)
+    let ignoredEpicShips = new Set([
+            "cr90corvette",
+            "croccruiser",
+            "gozanticlasscruiser",
+            "gr75mediumtransport",
+            "raiderclasscorvette",
+        ]),
+        knownMissingShips = new Set([
+            "tiesffighter",
+        ]),
+        fontShipNames = new Set(Object.keys(getFontMap('ships'))),
+        xwdataUrlBase = 'https://raw.githubusercontent.com/guidokessels/xwing-data2/master/'
 
-    // Get map of ship icons
-    var shipMap = getFontMap('ships');
-
-    // Get X-Wing database for ships
-    request('https://github.com/guidokessels/xwing-data/raw/master/data/ships.js', function (error, response, body) {    
-
-        var allShipData = JSON.parse(body);
-
-        // Check that all ships in the database have an icon
-        allShipData.forEach(function(shipData) {
-            var name = shipData['xws'];
-
-            // Some huge ships are encoded as [name]aft and [name]fore to represent front and back.
-            // Only check these once.
-            if (shipData['size'] == 'huge') {
-                if (name.endsWith('fore')) {
-                    name = name.substring(0, name.length - 4);
-                } else if (name.endsWith('aft')) {
-                    return;
-                }
-            }
-            
-            // This should only generate a warning
-            if (shipMap[name] == undefined) {
-                console.warn(("    WARNING: '" + name + "' missing ship icon.").yellow);
-            }
-            delete shipMap[name];
-        });
-
-        // Check that all icon names have a ship in the database
-        // assert.equal(shipMap.length, 0, "The following ships do not exist in the x-wing database: " + Object.keys(shipMap))
-        for (var name in shipMap) {
-            if (shipAliases.indexOf(name) === -1) {
-                assert.ok(false, "'" + name + "' is not in the x-wing ships database. Possible duplicate or typo?");
+    // Get all ship JSON paths
+    fetch(xwdataUrlBase + 'data/manifest.json')
+    .then(resp => resp.json())
+    .then(manifest => {
+        let shipfiles = []
+        for (let factionData of manifest.pilots) {
+            shipfiles = shipfiles.concat(factionData.ships)
+        }
+        // Fetch ship data in parallel
+        return Promise.all(shipfiles.map(path =>
+            fetch(xwdataUrlBase + path)
+            .then(r => r.json())
+            .then(shipdata => canonicalize(shipdata.name))
+        ))
+    })
+    .then(dataShipNames => {
+        // TODO: huge ships, when they arrive
+        dataShipNames = new Set(dataShipNames)
+        for (let name of dataShipNames) {
+            if (!fontShipNames.has(name)) {
+                console.warn((`    WARNING: '${name}' missing ship icon.`).yellow);
             }
         }
-        
-        done();
-    });
+        for (let name of fontShipNames) {
+            if (!dataShipNames.has(name) && !ignoredEpicShips.has(name)) {
+                if (knownMissingShips.has(name)) {
+                    console.warn((`    WARNING: icon exists for '${name}' which is known not to exist in xwing-data2`).yellow)
+                } else {
+                    assert.ok(false, `'${name}' is not in the x-wing ships database. Possible duplicate or typo?`);
+                }
+            }
+        }
+    })
+    .catch(err => {
+        assert.ok(false, `failed fetching ship data from xwing-data2: ${err}`)
+        done()
+    })
 };
 
 if (module == require.main) require('test').run(exports)
